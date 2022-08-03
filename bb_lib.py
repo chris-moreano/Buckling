28ff76c5cc21c66adef8e673ecb19e1d01e42be7
Christian Moreano <moreano.chris@gmail.com>
2022-08-03 09:34:54 -0700

    First git commit/backup

---
+++ bb_lib.py
@@ -0,0 +1,344 @@
+#import numpy as np
+import numpy as np
+from fpdf import FPDF
+from io import BytesIO
+import base64
+import math
+#import urllib
+
+# ==============================================================
+# Given Constants / Calculations
+# Below you will find given constants and some calculations that
+# are given in the google spreeadsheet thats posted above
+# the dictionaries spec_dic/buck_dic are containers filled with
+# given data for some samples.
+# ==============================================================
+# CONSTANTS FOR BENDING
+mandrel_Diameter =0.04450                               #m
+laminate_Thickness = 0.001120                           #m
+#span_Var = 1.686                                        #m
+inside_radius = mandrel_Diameter/2                      #m
+outside_radius = laminate_Thickness + inside_radius     #m
+
+# Second Moment of Area
+moment_area =  (math.pi/ 4*(outside_radius**4 - inside_radius**4))   #m^2                                   
+
+# Cross Sectional Area
+cross_sectional_area = math.pi * (outside_radius**2 - inside_radius**2) #m^4
+
+
+def find_roots(x,y):
+    np.seterr(divide = 'ignore')
+    s = np.abs(np.diff(np.sign(y))).astype(bool)
+    value = x[:-1][s] + np.diff(x)[s]/(np.abs(y[1:][s]/y[:-1][s])+1)
+    np.seterr(divide = 'warn') 
+    return value
+    
+def find_nearest(array, value):
+    #array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+
+# FORMULAS/EQUATIONS
+"""
+Formulas and constants can be found in this excel spreadsheet made by Dan Foster
+MT061 - CFRP Buckling Post-Hoop Damage 
+(Under buckling strees calcs tab)
+https://docs.google.com/spreadsheets/d/1E_udTrTWaQ_PSnOER0Ssk3IjKFw-yfmca2awrDQ3A14/edit#gid=1005744455
+"""
+def peak_eccentricity(displacement,span):  
+    return float((2*math.sqrt(displacement*span))/math.pi)
+
+def eccentricity_fail_location(peak_e,fail_loc,span):
+    return peak_e*math.sin((math.pi*fail_loc)/span)
+
+def axial_stress(load_at_fail,cs_area):
+    return load_at_fail/cs_area
+
+def bending_moment(load_at_fail,eccent_at_fail):
+    return load_at_fail*eccent_at_fail
+
+def bending_stress(outside_radius,bending_moment,second_mnt_are):
+    return ((bending_moment*outside_radius)/second_mnt_are)
+
+# ==============================================================
+# Set up FPDF class
+# ==============================================================
+# BEGIN Dictionary Value Adder
+class PDF(FPDF):
+    def header(self):
+        # Logo
+        #self.image('LtaLogo.png', 10, 8, 33)
+        # Arial bold 15
+        self.set_font('helvetica', 'B', 20)
+        # Move to the right
+        self.cell(80)
+        
+        # Title - Insert tittle in the code below.
+        self.cell(40, 10, "MT061R&D Tube Characterization - Report", 0, 0, 'C')
+        # Line break
+        self.ln(20)
+
+    # Page footer
+    def footer(self):
+        # Position at 1.5 cm from bottom
+        self.set_y(-15)
+        # Arial italic 8
+        self.set_font('helvetica', 'I', 8)
+        # Page number
+        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
+
+    def load_resource(self, reason, filename):
+        if reason == "image":
+            if filename.startswith("http://") or filename.startswith("https://"):
+                f = BytesIO(urlopen(filename).read())
+            elif filename.startswith("data"):
+                f = filename.split('base64,')[1]
+                f = base64.b64decode(f)
+                f = io.BytesIO(f)
+            else:
+                f = open(filename, "rb")
+            return f
+        else:
+            self.error("Unknown resource loading reason \"%s\"" % reason)
+
+
+    def create_table(self, table_data, title='', data_size = 10, title_size=12, align_data='L', align_header='L', cell_width='even', x_start='x_default',emphasize_data=[], emphasize_style=None,emphasize_color=(0,0,0)): 
+        """
+        table_data: 
+                    list of lists with first element being list of headers
+        title: 
+                    (Optional) title of table (optional)
+        data_size: 
+                    the font size of table data
+        title_size: 
+                    the font size fo the title of the table
+        align_data: 
+                    align table data
+                    L = left align
+                    C = center align
+                    R = right align
+        align_header: 
+                    align table data
+                    L = left align
+                    C = center align
+                    R = right align
+        cell_width: 
+                    even: evenly distribute cell/column width
+                    uneven: base cell size on lenght of cell/column items
+                    int: int value for width of each cell/column
+                    list of ints: list equal to number of columns with the widht of each cell / column
+        x_start: 
+                    where the left edge of table should start
+        emphasize_data:  
+                    which data elements are to be emphasized - pass as list 
+                    emphasize_style: the font style you want emphaized data to take
+                    emphasize_color: emphasize color (if other than black) 
+        
+        """
+        default_style = self.font_style
+        if emphasize_style == None:
+            emphasize_style = default_style
+        # default_font = self.font_family
+        # default_size = self.font_size_pt
+        # default_style = self.font_style
+        # default_color = self.color # This does not work
+
+        # Get Width of Columns
+        def get_col_widths():
+            col_width = cell_width
+            if col_width == 'even':
+                col_width = self.epw / len(data[0]) - 1  # distribute content evenly   # epw = effective page width (width of page not including margins)
+            elif col_width == 'uneven':
+                col_widths = []
+
+                # searching through columns for largest sized cell (not rows but cols)
+                for col in range(len(table_data[0])): # for every row
+                    longest = 0 
+                    for row in range(len(table_data)):
+                        cell_value = str(table_data[row][col])
+                        value_length = self.get_string_width(cell_value)
+                        if value_length > longest:
+                            longest = value_length
+                    col_widths.append(longest + 4) # add 4 for padding
+                col_width = col_widths
+
+
+
+                        ### compare columns 
+
+            elif isinstance(cell_width, list):
+                col_width = cell_width  # TODO: convert all items in list to int        
+            else:
+                # TODO: Add try catch
+                col_width = int(col_width)
+            return col_width
+
+        # Convert dict to lol
+        # Why? because i built it with lol first and added dict func after
+        # Is there performance differences?
+        if isinstance(table_data, dict):
+            header = [key for key in table_data]
+            data = []
+            for key in table_data:
+                value = table_data[key]
+                data.append(value)
+            # need to zip so data is in correct format (first, second, third --> not first, first, first)
+            data = [list(a) for a in zip(*data)]
+
+        else:
+            header = table_data[0]
+            data = table_data[1:]
+
+        line_height = self.font_size * 2.5
+
+        col_width = get_col_widths()
+        self.set_font(size=title_size)
+
+        # Get starting position of x
+        # Determin width of table to get x starting point for centred table
+        if x_start == 'C':
+            table_width = 0
+            if isinstance(col_width, list):
+                for width in col_width:
+                    table_width += width
+            else: # need to multiply cell width by number of cells to get table width 
+                table_width = col_width * len(table_data[0])
+            # Get x start by subtracting table width from pdf width and divide by 2 (margins)
+            margin_width = self.w - table_width
+            # TODO: Check if table_width is larger than pdf width
+
+            center_table = margin_width / 2 # only want width of left margin not both
+            x_start = center_table
+            self.set_x(x_start)
+        elif isinstance(x_start, int):
+            self.set_x(x_start)
+        elif x_start == 'x_default':
+            x_start = self.set_x(self.l_margin)
+
+
+        # TABLE CREATION #
+
+        # add title
+        if title != '':
+            self.multi_cell(0, line_height, title, border=0, align='j', ln=3, max_line_height=self.font_size)
+            self.ln(line_height) # move cursor back to the left margin
+
+        self.set_font(size=data_size)
+        # add header
+        y1 = self.get_y()
+        if x_start:
+            x_left = x_start
+        else:
+            x_left = self.get_x()
+        x_right = self.epw + x_left
+        if  not isinstance(col_width, list):
+            if x_start:
+                self.set_x(x_start)
+            for datum in header:
+                self.multi_cell(col_width, line_height, datum, border=0, align=align_header, ln=3, max_line_height=self.font_size)
+                x_right = self.get_x()
+            self.ln(line_height) # move cursor back to the left margin
+            y2 = self.get_y()
+            self.line(x_left,y1,x_right,y1)
+            self.line(x_left,y2,x_right,y2)
+
+            for row in data:
+                if x_start: # not sure if I need this
+                    self.set_x(x_start)
+                for datum in row:
+                    if datum in emphasize_data:
+                        self.set_text_color(*emphasize_color)
+                        self.set_font(style=emphasize_style)
+                        self.multi_cell(col_width, line_height, datum, border=0, align=align_data, ln=3, max_line_height=self.font_size)
+                        self.set_text_color(0,0,0)
+                        self.set_font(style=default_style)
+                    else:
+                        self.multi_cell(col_width, line_height, datum, border=0, align=align_data, ln=3, max_line_height=self.font_size) # ln = 3 - move cursor to right with same vertical offset # this uses an object named self
+                self.ln(line_height) # move cursor back to the left margin
+        
+        else:
+            if x_start:
+                self.set_x(x_start)
+            for i in range(len(header)):
+                datum = header[i]
+                self.multi_cell(col_width[i], line_height, datum, border=0, align=align_header, ln=3, max_line_height=self.font_size)
+                x_right = self.get_x()
+            self.ln(line_height) # move cursor back to the left margin
+            y2 = self.get_y()
+            self.line(x_left,y1,x_right,y1)
+            self.line(x_left,y2,x_right,y2)
+
+
+            for i in range(len(data)):
+                if x_start:
+                    self.set_x(x_start)
+                row = data[i]
+                for i in range(len(row)):
+                    datum = row[i]
+                    if not isinstance(datum, str):
+                        datum = str(datum)
+                    adjusted_col_width = col_width[i]
+                    if datum in emphasize_data:
+                        self.set_text_color(*emphasize_color)
+                        self.set_font(style=emphasize_style)
+                        self.multi_cell(adjusted_col_width, line_height, datum, border=0, align=align_data, ln=3, max_line_height=self.font_size)
+                        self.set_text_color(0,0,0)
+                        self.set_font(style=default_style)
+                    else:
+                        self.multi_cell(adjusted_col_width, line_height, datum, border=0, align=align_data, ln=3, max_line_height=self.font_size) # ln = 3 - move cursor to right with same vertical offset # this uses an object named self
+                self.ln(line_height) # move cursor back to the left margin
+        y3 = self.get_y()
+        self.line(x_left,y3,x_right,y3)
+
+def get_property_names():
+    return ["Mandrel Diameter[m]",
+                        "Laminate Thickness[m]",
+                        "Span[m]",
+                        "Failure Span Location[m]",
+                        "Post Buckled Displacement[m]",
+                        "Load at Failure[lbs]",
+                        "Outside Radius[m]",
+                        "Inside Radius[m]",
+                        "Cross Sectional Area[m^2]",
+                        "Second Moment of Area[m^4]",
+                        "Peak Eccentricity[m]",
+                        "Eccentricity at Failure Location[m]",
+                        "Axial Stress[Pa]",
+                        "Bending Moment[Nm]",
+                        "Bending Stress[Pa]",
+                        "Peak Laminate Failure Stress[Mpa]"]
+
+
+#Break from END LIST 
+break_dic = {
+                #KEY : [ VA1 , VAL2]
+                #Sample : [ span , brek from end ] all values converted from mm to m
+                "KF021810":[1.8,0.89],
+                "KF021821":[1.8,0.95],
+                "KF021854":[1.799,0.915],
+                "KF021855":[1.798,0.915],
+                "KF021860":[1.799,0.9],
+                "KF021861":[1.8,0.86],
+                "KF021868":[1.8,0.95],
+                "KF021870":[1.8,0.95],
+                "KF021885":[1.8,1],
+                "KF021887":[1.798,0.875],
+                "KF021889":[1.8,0.905],
+                "KF021890":[1.798,0.85],
+                "KF021915":[1.799,0.915],
+                "KF021923":[1.799,0.895],
+                "KF021925":[1.797,0.905],
+                "KF021927":[1.799,0.885],
+                "KF021932":[1.799,0.875],
+                "KF021943":[1.798,0.905],
+                "KF021944":[1.798,0.876],
+                "KF021947":[1.8,1.01],
+                "KF021959":[1.799,0.91],
+                "KF022016":[1.798,0.915],
+                "KF022048":[1.8,0.88],
+                "KF022049":[1.8,0.895],
+                "KF022083":[1.798,0.905]
+
+}
---
+++ buckle.py
@@ -0,0 +1,260 @@
+import os #used to crawl into directories to find files
+import pandas as pd
+import numpy as np
+from io import BytesIO
+from fpdf import FPDF
+pd.options.mode.chained_assignment = None  # default='warn'
+import matplotlib.pyplot as plt
+
+from bb_lib import *
+# ==============================================================
+#Iterate through Parent Directory
+#Generates Dictionary with Batches as Key, Filepaths as values
+# INPUT: None
+# OUTPUT: [[filename,filepath],[filename2,path2]]
+# ==============================================================
+
+decimal_places = 9
+cwd = os.getcwd()
+listofcsvs = []
+graphTitle = []
+pathcsv = []
+item_dic = {'Properties\Sample': ["Mandrel Diameter[m]",
+                        "Laminate Thickness[m]",
+                        "Span[m]",
+                        "Failure Span Location[m]",
+                        "Post Buckled Displacement[m]",
+                        "Load at Failure[lbs]",
+                        "Outside Radius[m]",
+                        "Inside Radius[m]",
+                        "Cross Sectional Area[m^2]",
+                        "Second Moment of Area[m^4]",
+                        "Peak Eccentricity[m]",
+                        "Eccentricity at Failure Location[m]",
+                        "Axial Stress[Pa]",
+                        "Bending Moment[Nm]",
+                        "Bending Stress[Pa]",
+                        "Peak Laminate Failure Stress[Mpa]"]}
+property_list = []
+bytes_list = []
+for subdir, dirs, files in os.walk(cwd):
+    
+    for file in files:
+        filepath = subdir + os.sep + file
+        if file.endswith(".csv") and not file.endswith("Events.csv"):
+            if file.startswith("M"):
+                if os.path.getsize(filepath) > 10000: # Magic number to filter out empty CSVs , 10000 bytes = 10kb
+                    tempString = file.translate({ ord(c): None for c in "-" }) 
+                    listofcsvs.append(tempString)
+                    pathcsv.append(filepath)
+                    graphTitle.append(tempString[6:14])
+                    #listofcsvs.append([tempString, filepath])
+                    #print("file: " + file + " Filepath: " + filepath)
+
+
+print (pathcsv)
+
+#print(len(listofcsvs))
+
+for iname,files in enumerate(pathcsv):
+    with open(files) as file_name:
+        
+                #if filename is a csv file... process data file
+        if ".csv" in files:
+            #print(datafile)
+            file_data = pd.read_csv(file_name)
+            #some files dont have the S:load column but Ch:load, 
+            # if found, rename and reassign to dataframe
+            tempString = "Ch:Load (lbs)"
+            if tempString in file_data.columns:
+                file_data = file_data.rename(columns={"Ch:Load (lbs)": "S:Load (lbs)"})
+     
+            file_data=file_data[["S:Load (lbs)","S:Position (in)","Time (sec)"]]
+            file_data = file_data.loc[:,~file_data.columns.duplicated()]
+        
+    
+    file_data.drop(file_data.columns.difference(["S:Load (lbs)","S:Position (in)","Time (sec)"]), 1, inplace=True)
+    load = file_data["S:Load (lbs)"].values
+    position = file_data["S:Position (in)"].values
+
+    loadmax = np.amax(load) #Find largest peak in Load
+    max_len = np.amax(position)
+
+    #print(loadmax)
+    #print(max_len)
+
+        # Process Data for Buckling
+        # All data seems similar
+        # and area to be analyzes is between 4000 and 6000 pounds
+        # save this portion of data and perform analysis
+    slope_data = file_data.loc[(file_data["S:Load (lbs)"] >= 4000) & (file_data["S:Load (lbs)"] < 6000)]
+        
+    #print("RTD Slope Data")
+    #print(slope_data)
+    slope_data["S:Load (lbs)"] = slope_data["S:Load (lbs)"].values
+    slope_data["S:Position (in)"] = slope_data["S:Position (in)"].values
+
+    # Slope Data of initial slope
+    slope_m,slope_b = np.polyfit(slope_data["S:Load (lbs)"],slope_data["S:Position (in)"],1)
+
+    print(slope_m)
+    print(slope_b)
+
+
+        # Calculate the percent change of all load values,  pc = x2-x1/x1 
+    file_data["%_Change"] = file_data["S:Load (lbs)"].pct_change(periods = 1)
+
+    last_ten = file_data.iloc[-10:]
+     
+    last_ten["%_Change"] = last_ten["%_Change"].abs()
+
+
+        # Look for percent change in between .20 and 1
+        # save index to last_point
+    last_point = last_ten.loc[(last_ten["%_Change"] >= .20) & (last_ten["%_Change"] <= 1.0009)].index
+
+        # Find the point before the drop 
+    last_p_value = file_data.iloc[last_point-1].values[0][0]
+
+    number_rows = len(file_data.index)
+
+        # take the data and find the roots of the intersecting lines.
+    z = find_roots(position,load-last_p_value)
+        #print(z)
+
+        # this is not the real value#
+    # INITIAL AND FINAL POSITION
+    buckle_initial = z[0]
+    buckle_final = z[len(z)-1]
+
+
+    print("\nInitial Position: ",buckle_initial)
+    print("Final Position: ",buckle_final)
+
+    load_initial = file_data[file_data["S:Position (in)"]== find_nearest(position,buckle_initial)].index
+    load_final = file_data[file_data["S:Position (in)"]== find_nearest(position,buckle_final)].index
+
+        #maxindex = np.where(loadpeaks == loadmax) #Find index of largest peak
+    #FINAL and INITIAL TIME
+    initial_time = float(file_data["Time (sec)"].iloc[load_initial].values)
+    final_time = float(file_data["Time (sec)"].iloc[load_final].values)
+
+    #FINAL AND INITIAL LOAD
+    load_initial = round(float(file_data["S:Load (lbs)"].iloc[load_initial].values),3)
+    load_final = round(float(file_data["S:Load (lbs)"].iloc[load_final].values),3)
+
+
+
+
+    print("Initial Time: ",initial_time)
+    print("Final Time: ",final_time)
+
+    rate_change = (buckle_final-buckle_initial)/(final_time-initial_time)
+
+    print("Initial Load: " ,load_initial)
+    print("Final Load: ",load_final)
+    
+    span_Var = float(break_dic[graphTitle[iname]][0])
+    p_e = peak_eccentricity((float(buckle_final)-buckle_initial)*.0254,span_Var)
+    eal = eccentricity_fail_location(p_e,float(break_dic[graphTitle[iname]][1]),span_Var)
+    a_s = axial_stress(load_final*4.44822,cross_sectional_area)
+    b_m = bending_moment(load_final*4.4482,eal)
+    b_s = bending_stress(outside_radius,b_m,moment_area)
+    plfs = (a_s + b_s)/1000000
+
+    property_list = [str(round(mandrel_Diameter,decimal_places)),str(round(laminate_Thickness,decimal_places)),str(span_Var),str(round(break_dic[graphTitle[iname]][1],decimal_places)),
+                    str(round(rate_change,decimal_places)),str(round(load_final,decimal_places)),str(round(outside_radius,decimal_places)),str(round(inside_radius,decimal_places)),str(round(cross_sectional_area,decimal_places)),str('{:.5e}'.format(moment_area)),
+                    str(round(p_e,decimal_places)),str(round(eal,decimal_places)),str(round(a_s,5)),str(round(b_m,decimal_places)),str(round(b_s,5)),str(round(plfs,decimal_places))]
+   
+    #property_list.append(10)
+    #print(property_list)
+
+    item_dic[graphTitle[iname]] = property_list
+
+    #property_list.clear()
+
+            #Graph
+    fig = plt.figure(figsize=(10,5))
+
+    plt.title(graphTitle[iname])
+    plt.ylabel("Load [lbs]")
+    plt.xlabel("Crosshead Displacement [in]")
+    plt.plot(position,load , label = "Load VS Position")
+
+        #plt.plot(position,slope_m*position, label ='Linear Regression')
+        #plt.axline((0,slope_b),slope =slope_m, color='black', label='by slope',linewidth=10)
+    plt.plot(slope_data["S:Position (in)"], slope_m*slope_data["S:Position (in)"] + slope_b, color = "black",linewidth = 5) 
+    plt.axhline(last_p_value,color= "orange", linestyle="-", label = "Drop Value")
+    plt.axvline(buckle_initial,color = 'red', linestyle ='dotted', label = "Initial Intersection")
+    plt.axvline(buckle_final,color = 'green', linestyle ='dotted',label = "Final Intersection")
+    plt.legend()
+    if loadmax < 200:
+        plt.ylim(0,200)
+        
+    else:
+        plt.ylim(200,10000)
+    plt.xlim(0,max_len+.05)
+    plt.tick_params(labelsize = 10)
+
+    #plt.annotate( "Initial Position: " + str(round(buckle_initial,4)) , xy = (.1,-.2), xycoords='axes fraction')
+    #plt.annotate( "Final Load:" + str(load_final) + ", Final Position: " + str(round(buckle_final,4)), xy = (.7,-.2), xycoords='axes fraction')
+
+    #plt.annotate( "\u0394Position/\u0394Time (m/sec) = " + str((rate_change)), xy = (.1,-.25), xycoords='axes fraction')
+    #plt.annotate( "Temperature(\u00b0F) : " + temperature, xy = (.7,-.25), xycoords='axes fraction')
+
+    #plt.annotate( "Sample Length(mm): " + tube_length, xy = (.1,-.3), xycoords='axes fraction')
+   # plt.annotate( "Break from end distance(mm): " + break_end, xy = (.7,-.3), xycoords='axes fraction')
+    plt.grid()
+    bio = BytesIO()
+    fig.savefig(bio, format="png", bbox_inches='tight')
+    bytes_list.append(bio)
+
+    #print(bio)
+
+
+
+    #or figure in bytes_list:
+
+
+max_page_width = 270
+
+    # Instantiation of inherited class
+pdf = PDF()
+
+    # Calls total page number for footer
+pdf.alias_nb_pages()
+
+    # Adds page because there is currently no page
+pdf.add_page()
+
+    #Set fonts for pdf
+pdf.set_font('helvetica', 'B', 7)
+pdf.set_fill_color(225,225,234)
+pdf.set_font("Times", size=10)
+
+pdf.create_table(table_data = item_dic,title='Bending Stress Calculations', cell_width='even' , emphasize_data= get_property_names(), emphasize_style='BIU',emphasize_color=(0,0,0))
+pdf.ln()
+pdf.ln()
+pdf.ln()
+#pdf.create_table(table_data = data,title='my title is the best title',cell_width='uneven',x_start=25)
+#pdf.ln()
+#pdf.create_table(table_data = data,title='my title is the best title',cell_width=22,x_start=50) 
+#pdf.ln()
+
+#pdf.create_table(table_data = data_as_dict,title='Is my text red',align_header='R', align_data='R', cell_width=[15,15,10,45,], x_start='C', emphasize_data=['45','Jules'], emphasize_style='BIU',emphasize_color=(255,0,0))
+#pdf.ln()
+
+
+#pdf.create_table(table_data = item_dic,align_header='R', align_data='R', cell_width=[15,15,10,45,], x_start='C') 
+for index, figure in enumerate(bytes_list):
+
+    pdf.image(name='data://image/png;base64,' + base64.b64encode(figure.getvalue()).decode()  , x =20 , w=170, h=120, type='png')
+
+
+pdf.output('MT061_Batch_1.pdf')
+
+# Need to create obejct as pdf
+print(item_dic)
+#pdf.output('table_with_cells.pdf')
+
+#print(item_dic)
